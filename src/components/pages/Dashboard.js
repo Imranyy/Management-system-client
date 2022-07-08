71a7175d6149579df0e7a57d65aca9e5ade7a020
imranyy <imranmatano6@gmail.com>
2022-07-08 13:28:28 +0300

    now1

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -161,6 +161,8 @@ const Dashboard=({setAuth})=>{
          <p className="light customfont" style={{fontSize: '12px'}}>User id: {id}</p>
          </div>
          <ul>
+          <li> <Link to='/user'><i className='material-icons left'>contact_page</i><h5 className='light customfont black-text' style={{marginRight:'600px'}}>Personal Info</h5></Link></li>
+          <li> <Link to='/Amount'><i className='material-icons left'>account_circle</i><h5 className='light customfont black-text' style={{marginRight:'600px'}}>Account</h5></Link></li>
          <li> <Link to='/user'><i className='material-icons left'>history</i><h5 className='light customfont black-text' style={{marginRight:'600px'}}>History</h5></Link></li>
          <li> <Link to='/Amount'><i className='material-icons left'>payments</i><h5 className='light customfont black-text' style={{marginRight:'600px'}}>Payment</h5></Link></li>
          <li> <Link to='/about'><i className='material-icons left'>help</i><h5 className='light customfont black-text' style={{marginRight:'600px'}}>Help</h5></Link></li>
